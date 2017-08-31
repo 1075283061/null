b631fda2aa7a02dfd83dd2066666b71e8d1f2c52
shynome <shynome@gmail.com>
2017-08-31 21:57:41 +0800

    init

---
+++ index.php
@@ -0,0 +1,7 @@
+<?require_once "./head.php";?>
+<div>
+  666666
+  <?require_once "./extra.php";?>
+</div>
+<?require_once "./ul.php";?>
+<?require_once "./foot.php";?>
---
+++ ul.php
@@ -0,0 +1,9 @@
+<ul>
+<?
+foreach(array(1,2,4,6) as $val):
+?>
+  <li><?=$val?></li>
+<?
+endforeach
+?>
+</ul>
